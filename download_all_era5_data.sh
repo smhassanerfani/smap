1de278a42584a40afb81df26036c8efb97cf6917
smhassanerfani <serfani@email.sc.edu>
2024-11-22 10:28:42 -0500

    feat(ERA5): add if statement to check if file exist before sending download request

--- download_all_era5_data.sh
+++ download_all_era5_data.sh
@@ -8,15 +8,22 @@ cd /home/serfani/projects/smap/
 source /home/serfani/projects/smap/.venv/bin/activate
 
 # Loop through each year
-for year in {1991..2023}; do
+for year in {2004..2009}; do
     echo "Starting downloads for year $year"
 
     # Loop through each month
     for month in {1..12}; do
+        
         echo "Requesting data for $year-$month..."
-
-        # Run the Python script for the current year and month in the background
-        python download_era5_nc.py --year "$year" --month "$month" &
+        file="/home/serfani/serfani_data0/era5_daily/ERA5_data_${year}_$(printf "%02d" $month).zip"
+        
+        if [ -f "$file" ]; then
+            echo "$file exists."
+        else
+            # Run the Python script for the current year and month in the background
+            python download_era5_nc.py --year "$year" --month "$month" &
+        fi
+     
     done
 
     # Wait for all background processes (i.e., all 12 monthly downloads) to complete for the current year
